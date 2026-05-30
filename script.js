fda6c503090c0fc63054e6380171123bd29c6f41
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2026-05-30 17:47:18 +0100

    v1.0.6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Script
 // @namespace    https://github.com/yourname/repo
-// @version      1.0.0
+// @version      1.0.6
 // @description  My userscript
 // @match        https://example.com/*
 // @downloadURL  https://raw.githubusercontent.com/saucedontsauce/aids/main/dist/script.user.js
