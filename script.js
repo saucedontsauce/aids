456af0fc18f05c0a86efb5eeefdaa578cdf5bf00
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2026-05-30 17:57:20 +0100

    Test commit for versioning

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         Script
-// @namespace    https://github.com/yourname/repo
+// @name         Armoury Improved Display Script
+// @namespace    https://github.com/saucedontsauce/aids
 // @version      1.0.6
-// @description  My userscript
-// @match        https://example.com/*
+// @description  Torn Armoury Enhancement Tool
+// @match        https://www.torn.com/factions.php*
 // @downloadURL  https://raw.githubusercontent.com/saucedontsauce/aids/main/dist/script.user.js
 // @updateURL    https://raw.githubusercontent.com/saucedontsauce/aids/main/dist/script.meta.js
+// @license copyright Adam Auckland-Blaydes
 // ==/UserScript==
 
 (function () {
