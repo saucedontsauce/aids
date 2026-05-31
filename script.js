5ec8ceaefc8ad630776b8576971d1cceacd203bf
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2026-05-31 20:55:03 +0100

    Added colour gradient and progress bars. fucking sucked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Armoury Improved Display Script
 // @namespace    https://github.com/saucedontsauce/aids
-// @version      1.1.5
+// @version      1.1.6
 // @description  Torn Armoury Enhancement Tool
 // @match        https://www.torn.com/factions.php*
 // @license      copyright Adam Auckland-Blaydes
@@ -13,6 +13,282 @@
 // @downloadURL  https://update.greasyfork.org/scripts/580450/Armoury%20Improved%20Display%20Script.user.js
 // @updateURL    https://update.greasyfork.org/scripts/580450/Armoury%20Improved%20Display%20Script.meta.js
 // ==/UserScript==
+
+const ranges = {
+    "Achilles": {
+        min: 50,
+        max: 169
+    },
+    "Assassinate": {
+        min: 50,
+        max: 148
+    },
+    "Backstab": {
+        min: 30,
+        max: 96
+    },
+    "Beserk": {
+        min: 20,
+        max: 87
+    },
+    "Bleed": {
+        min: 20,
+        max: 72
+    },
+    "Blindfire": {
+        min: 15,
+        max: 20
+    },
+    "Blindside": {
+        min: 25,
+        max: 96
+    },
+    "Bloodlust": {
+        min: 10,
+        max: 18
+    },
+    "Burn": {
+        min: 30,
+        max: 50
+    },
+    "Comeback": {
+        min: 50,
+        max: 127
+    },
+    "Conserve": {
+        min: 25,
+        max: 49
+    },
+    "Cripple": {
+        min: 20,
+        max: 58
+    },
+    "Crusher": {
+        min: 50,
+        max: 133
+    },
+    "Cupid": {
+        min: 50,
+        max: 158
+    },
+    "Deadeye": {
+        min: 25,
+        max: 123
+    },
+    "Deadly": {
+        min: 2,
+        max: 10
+    },
+    "Demoralized": {
+        min: 20,
+        max: 23
+    },
+    "Disarm": {
+        min: 3,
+        max: 15
+    },
+    "Double-edged": {
+        min: 10,
+        max: 32
+    },
+    "Double Tap": {
+        min: 15,
+        max: 57
+    },
+    "Emasculate": {
+        min: 15,
+        max: 16
+    },
+    "Empower": {
+        min: 50,
+        max: 222
+    },
+    "Eviscerate": {
+        min: 15,
+        max: 34
+    },
+    "Execute": {
+        min: 15,
+        max: 29
+    },
+    "Expose": {
+        min: 7,
+        max: 21
+    },
+    "Freeze": {
+        min: 20,
+        max: 26
+    },
+    "Finale": {
+        min: 10,
+        max: 17
+    },
+    "Focus": {
+        min: 15,
+        max: 35
+    },
+    "Frenzy": {
+        min: 5,
+        max: 14
+    },
+    "Fury": {
+        min: 10,
+        max: 36
+    },
+    "Grace": {
+        min: 20,
+        max: 76
+    },
+    "Hazardous": {
+        min: 30,
+        max: 31
+    },
+    "Home Run": {
+        min: 50,
+        max: 93
+    },
+    "Irradiate": {
+        min: 100,
+        max: 100
+    },
+    "Laceration": {
+        min: 35,
+        max: 45
+    },
+    "Motivation": {
+        min: 15,
+        max: 35
+    },
+    "Paralyze": {
+        min: 5,
+        max: 18
+    },
+    "Parry": {
+        min: 50,
+        max: 92
+    },
+    "Penetrate": {
+        min: 25,
+        max: 49
+    },
+    "Plunder": {
+        min: 20,
+        max: 49
+    },
+    "Poisoned": {
+        min: 85,
+        max: 100
+    },
+    "Powerful": {
+        min: 15,
+        max: 49
+    },
+    "Proficience": {
+        min: 20,
+        max: 59
+    },
+    "Puncture": {
+        min: 20,
+        max: 57
+    },
+    "Quicken": {
+        min: 50,
+        max: 245
+    },
+    "Rage": {
+        min: 4,
+        max: 18
+    },
+    "Revitalize": {
+        min: 10,
+        max: 24
+    },
+    "Roshambo": {
+        min: 50,
+        max: 132
+    },
+    "Severe Burning": {
+        min: 100,
+        max: 100
+    },
+    "Shock": {
+        min: 0,
+        max: 100
+    },
+    "Sleep": {
+        min: 0,
+        max: 100
+    },
+    "Slow": {
+        min: 20,
+        max: 64
+    },
+    "Smash": {
+        min: 100,
+        max: 100
+    },
+    "Smurf": {
+        min: 1,
+        max: 5
+    },
+    "Specialist": {
+        min: 20,
+        max: 59
+    },
+    "Spray": {
+        min: 20,
+        max: 24
+    },
+    "Storage": {
+        min: 100,
+        max: 100
+    },
+    "Stricken": {
+        min: 30,
+        max: 99
+    },
+    "Stun": {
+        min: 10,
+        max: 40
+    },
+    "Suppress": {
+        min: 25,
+        max: 49
+    },
+    "Sure Shot": {
+        min: 3,
+        max: 11
+    },
+    "Throttle": {
+        min: 50,
+        max: 170
+    },
+    "Toxin": {
+        min: 30,
+        max: 44
+    },
+    "Warlord": {
+        min: 15,
+        max: 45
+    },
+    "Weaken": {
+        min: 20,
+        max: 63
+    },
+    "Wind-up": {
+        min: 125,
+        max: 221
+    },
+    "Wither": {
+        min: 20,
+        max: 63
+    },
+}
+function getRangeColour(value, min, max) {
+    const t = Math.max(0, Math.min(1, (value - min) / (max - min)));
+    const hue = t * 120; // 0 = red, 120 = green
+    return `hsl(${hue}, 100%, 50%)`;
+}
 
 const util = {
     _scriptInfo: null,
@@ -114,33 +390,59 @@ const scripts = {
 
                 clone.style.display = "flex"
                 clone.style.color = "#999999"
-
-                let text = ""
+                const output = document.createElement("li");
+                output.className = clone.className;
+                let count = 0;
 
                 for (const child of clone.children) {
                     console.log(child)
                     if (!child.className.includes("bonus-attachment-blank-bonus-25")) {
+                        count++;
                         const full = child.title
 
-                        const bonus = full.split("<b>")[1].split("</b>")[0]
-                        const percent = full.split("<br/>")[1].split(" ")[0]
+                        const bonusText = full.split("<b>")[1].split("</b>")[0]
+                        const percentText = full.split("<br/>")[1].split(" ")[0]
 
-                        console.log("Bonus:", bonus)
-                        console.log("Percent:", percent)
-                        if (text !== "") {
-                            text += "<br/>"
-                            const parent = el.parentElement, gp = parent.parentElement;
-                            gp.style.height = "60px"
-                            gp.children[1].style.overflow = "visible"
-                        }
-                        text += `${bonus}: ${percent}`
+                        console.log("Bonus:", bonusText)
+                        console.log("Percent:", percentText)
+                        const bonusRow = document.createElement("div");
 
+                        bonusRow.style.position = "relative"
+                        const pretext = document.createElement("span")
+                        pretext.textContent = bonusText;
+                        pretext.style.paddingLeft = "5px";
+                        bonusRow.appendChild(pretext)
+
+                        const percent = document.createElement("span");
+                        percent.className = "right";
+                        percent.textContent = percentText;
+                        percent.style.color = getRangeColour(Number(percentText.substring(0, percentText.length - 1)), ranges[bonusText].min, ranges[bonusText].max);
+
+
+
+                        bonusRow.appendChild(percent)
+
+                        const prog = document.createElement("progress");
+                        prog.className = "bonusprog";
+                        prog.style.width = "100%";
+                        prog.style.position = "absolute";
+                        prog.style.bottom = "0";
+                        prog.style.left = "0";
+                        prog.style.right = "0";
+                        prog.value = Number(percentText.substring(0, percentText.length - 1)) - ranges[bonusText].min
+                        prog.max = ranges[bonusText].max - ranges[bonusText].min
+                        bonusRow.appendChild(prog)
+
+                        output.appendChild(bonusRow);
                     }
                 }
+                if (count > 1) {
+                    const parent = el.parentElement, gp = parent.parentElement;
+                    gp.style.height = "60px"
+                    gp.children[1].style.overflow = "visible"
+                }
+                return output
 
-                //clone.textContent = "";
-                clone.innerHTML = text;
-                return clone
             }
             function iconMode(clone) {
                 let i = 0
@@ -247,6 +549,36 @@ function startObserver() {
 }
 
 function init() {
+    if (!document.getElementById("AIDSstyle")) {
+        const style = document.createElement("style");
+        style.id = "AIDSstyles"
+        style.textContent = `
+  .bonusprog{
+    appearance: none;
+    -webkit-appearance: none;
+    width: 300px;
+    height: 10px;
+    border-radius: 0;
+  }
+  /* Background */
+  .bonusprog::-webkit-progress-bar {
+    background: #ddd;
+    border-radius: 0;
+  }
+  /* Filled portion */
+  .bonusprog::-webkit-progress-value {
+    background: #4caf50;
+    border-radius: 0;
+  }
+  /* Firefox */
+  .bonusprog::-moz-progress-bar {
+    background: #4caf50;
+    border-radius: 0;
+  }
+`;
+        document.head.appendChild(style);
+    }
+
     if (!window.location.hash.includes("#/tab=armoury")) {
         return;
     }
